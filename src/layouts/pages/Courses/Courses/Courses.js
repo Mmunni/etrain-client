2aa39b4ad962b2f5af3f8c7f759d8bbe0df5b479
Mmunni <masumamunni20@gmail.com>
2022-10-27 01:26:04 -0700

    responsive-updated

--- src/layouts/pages/Courses/Courses/Courses.js
+++ src/layouts/pages/Courses/Courses/Courses.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { FaFileDownload } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 
 const Courses = () => {
@@ -9,6 +10,9 @@ const Courses = () => {
     return (
         <div>
              <div className="container my-5">
+                <div className='fs-2 text-end mb-5'>
+                <FaFileDownload/>
+                </div>
              <Card>
                  <Card.Img variant="top" src={image_url} />
                     <Card.Body>
