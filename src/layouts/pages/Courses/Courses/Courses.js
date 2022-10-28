3fa32f589876e45675b103c4ab29acb3734ff99b
Mmunni <masumamunni20@gmail.com>
2022-10-27 19:02:03 -0700

    pdf added

--- src/layouts/pages/Courses/Courses/Courses.js
+++ src/layouts/pages/Courses/Courses/Courses.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import { FaFileDownload } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
-import './Courses.css';
+import "./Courses.css";
+
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 const Courses = () => {
     const course = useLoaderData();
-    const { title, details, image_url} = course;
+     const { title, details, image_url} = course;
     return (
         <div>
-             <div className="container my-5">
-                <div className='fs-2 text-end mb-5'>
-                <FaFileDownload/>
-                </div>
-
-             <Card>
+            <div className="container">
+            <div className="pdf-style">
+        <Pdf targetRef={ref} filename="courses.pdf">
+        {({ toPdf }) => <button className='text-end mb-5 mt-3' onClick={toPdf}><FaFileDownload/>Generate Pdf</button>}
+        </Pdf>
+      <div ref={ref}>
+            <div className="row">
+                
+                <Card>
                  <Card.Img variant="top" src={image_url} />
                     <Card.Body>
                         <Card.Title>{title}</Card.Title>
@@ -23,9 +30,15 @@ const Courses = () => {
                              </Card.Text>
                     </Card.Body>
                 </Card>
-             </div>
+            </div>
+      </div>
+    </div>
+            </div>
+
+    
         </div>
     );
 };
+
 
 export default Courses;
